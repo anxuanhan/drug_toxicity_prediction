a0ef4ecf897a9137f5841a70ce4e290ded2f197a
anxuanhan <121469891+anxuanhan@users.noreply.github.com>
2023-07-24 12:28:35 +0800

    Update software.py

--- software.py
+++ software.py
@@ -5,12 +5,12 @@ import torch.nn as nn
 
 threshold = 0.5
 
-parser = argparse.ArgumentParser(description="输入csv文件的绝对路径")
-parser.add_argument('path', type=str, help='文件绝对路径')
+parser = argparse.ArgumentParser(description="Please enter the absolute path of the csv file")
+parser.add_argument('path', type=str, help="The absolute path to the file")
 args = parser.parse_args()
 path = args.path
 print(path)
-features = pd.read_pickle(path)
+features = pd.read_csv(path)
 features = torch.tensor(features)
 print(features.shape)
 
@@ -21,14 +21,12 @@ class Model(nn.Module):
         self.linear2 = nn.Linear(256, 128)
         self.linear3 = nn.Linear(128, 32)
         self.output = nn.Linear(32, out_features)
-
     def forward(self, x):
         x = torch.relu(self.linear1(x))
         x = torch.relu(self.linear2(x))
         x = torch.relu(self.linear3(x))
         out = torch.sigmoid(self.output(x))
         return out
-
 
 def test_label(yhat_list, threshold):
     for i in range(len(yhat_list)):
@@ -50,6 +48,3 @@ yhat_list.extend(yhat)
 
 yhat_label = test_label(yhat_list,threshold)
 print(yhat_label)
-
-
-
